c98be1909d8842136d83f179eaa6b0ba51f1c20b
Sina Deyhim <sina.deyhim@gmail.com>
2025-07-10 11:52:36 -0400

    adding tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     cors: {
-      origin: "*", // or specific: ['chrome-extension://<your-id>']
+      origin: "*",
     },
   },
 });
